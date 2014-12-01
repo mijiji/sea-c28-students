18ea2626da0a9bd28fb497c5044a4b679e84f2c9
Matthew Lee <ml7@uw.edu>
2014-12-01 14:33:38 -0800

    Adding behavior as described in the assignment and testing to verify.

--- Students/MatLee/session02/ack.py
+++ Students/MatLee/session02/ack.py
@@ -3,8 +3,18 @@ def ackermann(m, n):
     if m is 0:
         return n+1
     elif (n is 0) and (m > 0):
-        return "mode 2"
+        return ackermann(m-1, 1)
     else:
-        return "mode 3"
+        return ackermann(m-1, ackermann(m, n-1))
 
 
+if __name__ == "__main__":
+    m = [[1, 2, 3, 4, 5],
+         [2, 3, 4, 5, 6],
+         [3, 5, 7, 9, 11],
+         [5, 13, 29, 61, 125]]
+    for y in range(4):
+        for x in range(5):
+            assert ackermann(y, x) == m[y][x]
+        
+    print "All Tests Pass."
