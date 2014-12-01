4914a7f787e032d97374557b40a2bccaf59ac0ea
Henry Grantham <JustinKan@Justins-MacBook-Air.local>
2014-12-01 02:51:01 -0800

    All three functions done. Now need to do asserts.

--- Students/HenryGrantham/session02/series.py
+++ Students/HenryGrantham/session02/series.py
@@ -6,8 +6,8 @@
 
 def fibonacci(n):
     """Return nth number in a fionacci series
-       where n is an integer >= 0.
-       If n is less than 0, return None
+       where n is an integer > 0.
+       If n is less than 1, return None
     """
     if n == 1 :
         return 0
@@ -21,9 +21,9 @@ print (fibonacci(3))
 print (fibonacci(4))
 
 def lucas(n):
-    """Return nth number in a lucus number series
-       where n is an integer >= 0.
-       If n is less than 0, return None
+    """Return nth number in a lucas number series
+       where n is an integer >  0.
+       If n is less than 1, return None
     """
     if n == 1 :
         return 2
@@ -35,3 +35,20 @@ def lucas(n):
         return None
 print (lucas(3))
 print (lucas(4))
+
+def sum_series(n, a = 0, b = 1):
+    """Return nth number in a sum_series series
+       where n is an integer >  0.
+       If n is less than 1, return None
+       a and b are the first and second numbers in the series.
+    """
+    if n == 1 :
+        return a
+    elif n == 2:
+        return b
+    elif n > 2:
+        return sum_series(n-2, a, b) + sum_series(n-1, a, b)
+    else:
+        return None
+print (sum_series(3))
+print (sum_series(4))
