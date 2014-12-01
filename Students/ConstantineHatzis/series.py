354ba05ea0ae362c6542b3377a781ea3bf17782e
Constantine <constantine.hatzis@gmail.com>
2014-12-01 00:38:50 -0800

    Added sum_series series choosing function

--- Students/ConstantineHatzis/series.py
+++ Students/ConstantineHatzis/series.py
@@ -15,3 +15,13 @@ def lucas(n):
     for i in range(1, n):
         a, b = b, a + b  # Generate nth and nth + 1 value of the Lucas Numbers
     return (a)
+
+
+def sum_series(n, a=0, b=1):
+    """Return the nth value of a series determined by the optional arguments, a and b"""
+    if a == 0 and b == 1:
+        fibonacci(n)
+    elif a == 2 and b == 1:
+        lucas(n)
+    else:
+        print(u"That function is not available yet")
