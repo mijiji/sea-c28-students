19a8c648d6745bd6d62468a6f421e2793bfcd3c6
Constantine <constantine.hatzis@gmail.com>
2014-12-01 00:07:15 -0800

    Corrected fibonacci function to generate nth number in Fibonacci series

--- Students/ConstantineHatzis/series.py
+++ Students/ConstantineHatzis/series.py
@@ -4,9 +4,6 @@ from __future__ import print_function
 def fibonacci(n):
     """Return the nth number in the Fibonacci Series"""
     a, b = 0, 1  # Initialize Fibonacci Series
-    for i in range(n):
-        a, b = b, a + b
-        print(b)
-        return (b)
-
-fibonacci(5)
+    for i in range(1, n):
+        a, b = b, a + b  # Generate nth and nth + 1 number in Fibonacci Series
+    return (a)
