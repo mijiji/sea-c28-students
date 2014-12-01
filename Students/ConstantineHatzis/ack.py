d0a098d98d743f636f044ccfdb5e9467ba49b8eb
Constantine <constantine.hatzis@gmail.com>
2014-12-01 13:59:52 -0800

    Cleaned up code to keep lines less than 80 characters

--- Students/ConstantineHatzis/ack.py
+++ Students/ConstantineHatzis/ack.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 
 
 def ack(m, n):
-    """Return the ouput of the Ackermann function for two numbers greater than zero."""
+    """Return the ouput of the Ackermann function for two numbers greater than
+        zero."""
     if m < 0 or n < 0:
         result = None
     else:
@@ -14,15 +15,17 @@ def ack(m, n):
             result = ack(m-1, ack(m, n-1))
     return result
 
-# If ack is run with m > 3, the recurssion limit will be reached, unless it is m = 3, n = 0
+# If ack is run with m > 3, the recurssion limit will be reached, unless it is
+#  m = 3, n = 0
 
 if __name__ == "__main__":
     # Test values from the Wikipedia page for the Ackermann function
-    test_values = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 5, 7, 9, 11],
-                    [5, 13, 29, 61, 125]]
+    test_values = ([[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 5, 7, 9, 11],
+                    [5, 13, 29, 61, 125]])
 
     for m in range(4):
         for n in range(5):
             assert ack(m, n) == test_values[m][n]
-    # If an assert fails, and exception will be thrown and this following print will not executre.
+    # If an assert fails, and exception will be thrown and this following print
+    # will not executre.
     print(u"All Tests Pass")
