8483014d598ee88927fa9c5308658ecd11d0a9ad
Henry Grantham <JustinKan@Justins-MacBook-Air.local>
2014-12-01 02:40:25 -0800

    Fibonacci series and Lucas Number eseries work, but need to make Sum series function.

--- Students/HenryGrantham/session02/series.py
+++ Students/HenryGrantham/session02/series.py
@@ -5,17 +5,33 @@
 # starts with any two number.
 
 def fibonacci(n):
-    """Return nth number ina fionacci series
+    """Return nth number in a fionacci series
        where n is an integer >= 0.
        If n is less than 0, return None
     """
-    if n ==0 :
+    if n == 1 :
         return 0
-    elif n == 1:
+    elif n == 2:
         return 1
-    elif n > 1:
-        return fibonacci(n-1)
+    elif n > 2:
+        return fibonacci(n-2) + fibonacci(n-1)
     else:
         return None
 print (fibonacci(3))
 print (fibonacci(4))
+
+def lucas(n):
+    """Return nth number in a lucus number series
+       where n is an integer >= 0.
+       If n is less than 0, return None
+    """
+    if n == 1 :
+        return 2
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return lucas(n-2) + lucas(n-1)
+    else:
+        return None
+print (lucas(3))
+print (lucas(4))
