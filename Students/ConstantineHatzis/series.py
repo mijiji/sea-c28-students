c01e872c52628920cbe26a9a2e8f6e626c4dd6c3
Constantine <constantine.hatzis@gmail.com>
2014-12-01 00:11:12 -0800

    Added lucas function to find nth value of Lucas numbers and print statements to debug

--- Students/ConstantineHatzis/series.py
+++ Students/ConstantineHatzis/series.py
@@ -2,8 +2,20 @@ from __future__ import print_function
 
 
 def fibonacci(n):
-    """Return the nth number in the Fibonacci Series"""
+    """Return the nth value in the Fibonacci Series"""
     a, b = 0, 1  # Initialize Fibonacci Series
     for i in range(1, n):
-        a, b = b, a + b  # Generate nth and nth + 1 number in Fibonacci Series
+        a, b = b, a + b  # Generate nth and nth + 1 value in Fibonacci Series
     return (a)
+
+
+def lucas(n):
+    """Return the nth value of the Lucas Numbers"""
+    a, b = 2, 1  # Initialize Lucas Numbers
+    print(a)
+    for i in range(1, n):
+        a, b = b, a + b  # Generate nth and nth + 1 value of the Lucas Numbers
+        print(a)
+    return (a)
+
+lucas(5)
