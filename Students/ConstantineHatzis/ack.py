1e9983ef262153a86dd64b4ef2d8cfca9d65540c
Constantine <constantine.hatzis@gmail.com>
2014-11-30 23:15:57 -0800

    Debugged recursive ack function

--- Students/ConstantineHatzis/ack.py
+++ Students/ConstantineHatzis/ack.py
@@ -3,21 +3,16 @@ from __future__ import print_function
 
 def ack(m, n):
     """Return the ouput of the Ackermann function for two numbers greater than zero."""
-    print(m, n)
     if m < 0 or n < 0:
-        print(u"m < 0 and n < 0: ", m, n)
-        return None
-    elif m == 0:
-        print(u"m == 0: ", m, n, n + 1)
-        return n + 1
-    elif m > 0 and n == 0:
-        print(u"m > 0 and n == 0: ", m, n)
-        ack(m-1, 1)
-    elif m > 0 and n > 0:
-        print(u"m > 0 and n > 0: ", m, n)
-        ack(m-1, ack(m, n-1))
-
-print(u"Answer = ", ack(2, 0))
+        n = None
+    else:
+        if m == 0:
+            n = n + 1
+        elif m > 0 and n == 0:
+            n = ack(m-1, 1)
+        elif m > 0 and n > 0:
+            n = ack(m-1, ack(m, n-1))
+    return n
 
 if __name__ == "__main__":
     testValues = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 5, 7, 9, 11], [5, 13, 29, 61, 125]]
