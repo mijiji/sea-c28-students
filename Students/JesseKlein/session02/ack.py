7fbcb63585baaf59d056b96fc58811f1410a69aa
Jesse Klein <jklein08@gmail.com>
2014-12-01 17:01:21 -0800

    Saved changes to ack.py

--- Students/JesseKlein/session02/ack.py
+++ Students/JesseKlein/session02/ack.py
@@ -1,3 +1,23 @@
 def ack(m, n):
     """Return result of Ackermann function."""
+    if m == 0:
+        return n + 1
+    if m > 0 and n == 0:
+        return ack(m - 1, 1)
+    if m > 0 and n > 0:
+        return ack(m - 1, ack(m, n - 1))
+    if m < 0 or n < 0:
+        return none
+
+wiki = [1, 2, 3, 4, 5, 2, 3, 4, 5, 6, 3, 5, 7, 9, 11, 5, 13, 29, 61, 125]
+
+my_list = []
+
+if __name__ == "__main__":
+    for i in range(4):
+        for j in range(5):
+            my_list.append(ack(i, j))
+
+    if wiki == my_list:
+        print(u"All Tests Pass")
 
