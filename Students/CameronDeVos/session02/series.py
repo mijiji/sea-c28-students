3c324ab95c1e5067d6c0a39d2d6a7f770c49887d
Cameron de Vos <cameron2@outlook.com>
2014-12-01 17:19:28 -0800

    Add Lucas function

--- Students/CameronDeVos/session02/series.py
+++ Students/CameronDeVos/session02/series.py
@@ -8,3 +8,12 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
+def lucas(n):
+    """Return the nth value of the Lucas series."""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
