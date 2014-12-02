d914d372212c3a38a3b9c3aa380e5ce4023cb5f6
Constantine <constantine.hatzis@gmail.com>
2014-12-02 08:43:53 -0800

    Added user input and fruit list append

--- Students/ConstantineHatzis/list_lab.py
+++ Students/ConstantineHatzis/list_lab.py
@@ -2,6 +2,12 @@
 
 from __future__ import print_function
 
-fruit = [u'Apples', u'Pears', u'Oranges', u'Peaches']
+fruit = [u"Apples", u"Pears", u"Oranges", u"Peaches"]
+
+print(fruit)
+
+new_fruit = raw_input(u"Name another fruit to add to the list: ")
+
+fruit.append(new_fruit.decode("utf-8"))
 
 print(fruit)
