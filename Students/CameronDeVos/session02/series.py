264cdced5300ea38b211b361a422137264657cce
Cameron de Vos <cameron2@outlook.com>
2014-12-01 17:27:06 -0800

    Add sum_series function

--- Students/CameronDeVos/session02/series.py
+++ Students/CameronDeVos/session02/series.py
@@ -17,3 +17,19 @@ def lucas(n):
         return 1
     else:
         return lucas(n - 1) + lucas(n - 2)
+
+def sum_series(n, x=0, y=1):
+    """Return the nth value in a series based on its first two values.
+
+    Keyword arguments:
+    n -- the spot in series
+    x -- the 1st number in series, default aligned with fibonacci series
+    y -- the 2nd number in series, default aligned with fibonacci series
+    """
+    if n == 0:
+        return x
+    elif n == 1:
+        return y
+    else:
+        return sum_series(n - 1, x, y) + sum_series(n - 2, x, y)
+
