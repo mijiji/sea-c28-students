05be8755b9c2c2624488de2cd7cc0e01dcbf2305
Carly Slater <carlyslater@gmail.com>
2014-12-02 20:28:13 -0800

    completed list part 1, make list, append list, user input to index list, use + to add to list and use insert to add to list

--- Students/CarlySlater/list_lab.py
+++ Students/CarlySlater/list_lab.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 
 fruit = [u'apples', u'pears', u'oranges', u'peaches']
-print fruit[0]
-print fruit[1]
-print fruit[2]
-print fruit[3]
+for i in fruit:
+    print i
+
+
 #Instructors: can't find a better way to code this to print the list as a list of words, not u'oranges', u'apples', etc...
 
 fruit.append(raw_input(u'Please add a fruit:     '))
-print fruit[0]
-print fruit[1]
-print fruit[2]
-print fruit[3]
-print fruit[4]
+for i in fruit:
+    print i
+
+usernumber = input(u'choose a number from 1 to 5:     ')
+
+print usernumber
+print fruit[(usernumber-1)]
+
+print ""
+fruit = ([u'kiwi'] + fruit)
+for i in fruit:
+    print i
+print ""
+fruit.insert(0, u'pomelo')
+for i in fruit:
+    print i
+
+
