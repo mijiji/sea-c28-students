7a4028539a7b2e8100321558140b42753edbf901
Carly Slater <carlyslater@gmail.com>
2014-12-02 19:37:00 -0800

    Added create list, added append list with prompt, and print list again.  Having issues printing list efficiently.

--- Students/CarlySlater/list_lab.py
+++ Students/CarlySlater/list_lab.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python
+
+fruit = [u'apples', u'pears', u'oranges', u'peaches']
+print fruit[0]
+print fruit[1]
+print fruit[2]
+print fruit[3]
+#Instructors: can't find a better way to code this to print the list as a list of words, not u'oranges', u'apples', etc...
+
+fruit.append(raw_input(u'Please add a fruit:     '))
+print fruit[0]
+print fruit[1]
+print fruit[2]
+print fruit[3]
+print fruit[4]
