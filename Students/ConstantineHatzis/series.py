f9e1c148e56bc1a83ff3c5aa5d98d9c1f09e0d54
Constantine <constantine.hatzis@gmail.com>
2014-12-03 14:28:31 -0800

    Changed sum_series fucntion
    My initial fucntion threw out an error message when the two optional arguments
    didn't correspond to Fibonacci or Lucas. Now it generates the nth value of the
    series generated by the same code as Lucas and Fibonacci

--- Students/ConstantineHatzis/series.py
+++ Students/ConstantineHatzis/series.py
@@ -6,7 +6,7 @@ def fibonacci(n):
     a, b = 0, 1  # Initialize Fibonacci Series
     for i in range(1, n):
         a, b = b, a + b  # Generate nth and nth + 1 value in Fibonacci Series
-    return (a)
+    return a
 
 
 def lucas(n):
@@ -14,19 +14,17 @@ def lucas(n):
     a, b = 2, 1  # Initialize Lucas Numbers
     for i in range(1, n):
         a, b = b, a + b  # Generate nth and nth + 1 value of the Lucas Numbers
-    return (a)
+    return a
 
 
 def sum_series(n, a=0, b=1):
     """Return the nth value of a series determined by the optional arguments, a
         and b"""
-    if a == 0 and b == 1:
-        result = fibonacci(n)
-    elif a == 2 and b == 1:
-        result = lucas(n)
-    else:
-        print(u"That function is not available yet")
-    return result
+
+    for i in range(1, n):
+        a, b = b, a + b  # Generate nth and nth + 1 values
+    return a
+
 
 if __name__ == "__main__":
     # This is a selection of test cases for the Fibonacci Series, Lucas
@@ -41,7 +39,7 @@ if __name__ == "__main__":
     # The 5th value of the Fibonacci and Lucas Numbers, and an unknown
     # function
     test_values_sum_series = ([[5, 0, 1, 3], [5, 2, 1, 7],
-                              [5, 4, 3, "That function is not available yet"]])
+                              [5, 3, 10, 36]])
 
     # Test fibonacci() and lucas()
     for n in range(7):
@@ -49,7 +47,7 @@ if __name__ == "__main__":
         assert lucas(n + 1) == test_values_lucas[n]
 
     # Test sum_series()
-    for n in range(2):
+    for n in range(3):
         assert (sum_series(*test_values_sum_series[n][0:3]) ==
                 test_values_sum_series[n][3])
 
