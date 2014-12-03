cba5cd96d08d9383469439330822b18133717379
mijiji <michael.yu.ji@gmail.com>
2014-12-02 20:33:01 -0800

    added session01 and break_me.py

---
+++ Students/MichaelJ/session01/break_me.py
@@ -0,0 +1,38 @@
+Python 2.7.8 (v2.7.8:ee879c0ffa11, Jun 29 2014, 21:07:35) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+sqrt(9)
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+  
+'''
+sqrt(9)
+NameError: name 'sqrt' is not defined
+'''
+
+'''
+>>> a + b = 4
+SyntaxError: can't assign to operator
+'''
+
+'''
+>>> len(4,2,5)
+
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    len(4,2,5)
+TypeError: len() takes exactly one argument (3 given)
+'''
+
+>>> dog = 8
+>>> dog.upper()
+
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    dog.upper()
+AttributeError: 'int' object has no attribute 'upper'
+>>> 
+
