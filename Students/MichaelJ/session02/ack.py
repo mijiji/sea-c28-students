81d52f699809fc101455990a31f0d29bdfa26e95
mijiji <michael.yu.ji@gmail.com>
2014-12-03 18:12:28 -0800

    small fix 2, ackermann

--- Students/MichaelJ/session02/ack.py
+++ Students/MichaelJ/session02/ack.py
@@ -11,7 +11,7 @@ def A(m, n):
     # ackermann function does not account for negative numbers in input
     if m == 0:
         return n + 1
-    elif m > 0 and n > 0:
+    elif n == 0:
         return A(m - 1, 1)
     elif m > 0 and n > 0:
         return A(m - 1, A(m, n - 1))
