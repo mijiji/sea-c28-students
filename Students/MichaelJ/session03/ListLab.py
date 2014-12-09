9e98a00a789d813bfe160261c7e4f2fd2e3fa9fe
mijiji <michael.yu.ji@gmail.com>
2014-12-08 23:36:19 -0800

    series, finish part 4

--- Students/MichaelJ/session03/ListLab.py
+++ Students/MichaelJ/session03/ListLab.py
@@ -2,7 +2,7 @@
 list lab
 part 1
 '''
-list = ["Apples", "Pears", "Oranges", " Peaches"]
+list = ["Apples", "Pears", "Oranges", "Peaches"]
 
 print list
 
@@ -14,22 +14,23 @@ print list
 
 pick = int(raw_input(u"give a number for that spot's fruit "))
 
-print u"%f is in spot %p "%list(pick - 1), pick
+# print u"%i is in spot %s "%list[pick - 1], pick
+print u"{} is in spot {}".format(list[pick - 1], pick)
 
 print list
 
-list.insert("Durian", 0)
+list.insert(0, "Durian")
 
 print list
 
 print "fruits that start with P"
 for fruit in list:
-    if fruit[0] == "P"
-    print fruit
+    if fruit[0] == "P":
+        print fruit
 
 # part 2
-list2 = list
-list2.remove(Durian)
+list2 = list[:]
+list2.remove("Durian")
 
 print list2
 
@@ -37,5 +38,17 @@ vaporize = raw_input("Which fruit do you want to remove? ")
 
 list2.remove(vaporize)
 
+print list2
+
 # part 3
-list3 = list
+list3 = list[:]
+for fruit in list3:
+    like = raw_input(u"Do you like %s? " % fruit)
+    if "yes":
+        pass
+    elif "no":
+        list3.remove(fruit)
+    else:
+        print "only yes or no"
+print list3
+
