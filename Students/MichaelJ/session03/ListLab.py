15b5d643c76151aafedaecd83dc992ae256a9870
mijiji <michael.yu.ji@gmail.com>
2014-12-08 16:36:15 -0800

    series, finish part 1

--- Students/MichaelJ/session03/ListLab.py
+++ Students/MichaelJ/session03/ListLab.py
@@ -6,9 +6,23 @@ list = ["Apples", "Pears", "Oranges", " Peaches"]
 
 print list
 
-add = raw_input(u"add fruit")
+add = raw_input(u"add fruit ")
 
 list.append(add)
 
 print list
 
+pick = int(raw_input(u"give a number for that spot's fruit "))
+
+print u"%f is in spot %p"%(list(pick - 1, pick))
+
+print list
+
+list.inser("Durian", 0)
+
+print list
+
+print "fruits that start with P"
+for fruit in list:
+    if fruit[0] == "P"
+        print fruit
