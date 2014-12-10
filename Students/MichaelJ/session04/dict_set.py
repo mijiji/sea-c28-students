58f98adecbbd8e5b8044896eb8607210efcd0fb3
mijiji <michael.yu.ji@gmail.com>
2014-12-10 14:46:34 -0800

    dict_set, part 1

--- Students/MichaelJ/session04/dict_set.py
+++ Students/MichaelJ/session04/dict_set.py
@@ -7,3 +7,23 @@ This program creates a dictionary containing, "name", "city", "cake".
 You will be able to add and delete entries.
 """
 
+
+db = {
+    "name": "Chris",
+    "city": "Seattle",
+    "cake": "Chocolate"}
+
+print db
+
+del db["cake"]
+
+print db
+
+db["fruit"] = "Mango"
+
+print db
+
+print db.keys()
+
+print db.values()
+
