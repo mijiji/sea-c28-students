790325f5c4a35ec46ca0d16736c61833e243f793
mijiji <michael.yu.ji@gmail.com>
2014-12-09 18:11:55 -0800

    rot13 finished

--- Students/MichaelJ/session03/ROT13.py
+++ Students/MichaelJ/session03/ROT13.py
@@ -19,7 +19,20 @@ def rot13(message):
     encrypt = []
     for letter in message:
         if letter in string.ascii_uppercase:
-            b = A + ((ord(b) - A + 13) % 26)
+            b = A + ((ord(letter) - A + 13) % 26)
         elif letter in string.ascii_lowercase:
-            b = a + ((ord(b) - a + 13) % 26)
-        else b = ord(letter)
+            b = a + ((ord(letter) - a + 13) % 26)
+        else:
+            b = ord(letter)
+        encrypt.append(chr(b))
+    return "".join(encrypt)
+
+if __name__ == "__main__":
+
+    print rot13("Apple")
+    assert rot13("Apple") == "Nccyr"
+
+    print rot13("Hello!")
+    assert rot13("Hello1!") == "Uryyb1!"
+
+    print "all tests pass"
