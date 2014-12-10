505f8e1e9ae1b2d246a37bb322588281d703b021
mijiji <michael.yu.ji@gmail.com>
2014-12-10 00:53:20 -0800

    Added mailroom blueprint

---
+++ Students/MichaelJ/session03/MailBrainstorm.py
@@ -0,0 +1,44 @@
+"""
+Some pseudocode for planning mailroom madness.
+
+Planning mailroom madness
+
+This is not a program. Rather some code for planning
+mailroom madness.
+"""
+
+
+# main menu
+def main_menu():
+    choice = raw_input()
+"""
+1. send thank you
+2. create a report
+3. quit
+"""
+
+# Donor data base dictionary
+donor = {
+    "First Last": [342.23, 234.23],
+    "Santa Claus": [23.02]
+}
+
+# option 1
+"""
+ask for full name
+"""
+    choice1 = raw_input()
+    if choice1 = database:
+        print database
+        # with donors 
+        # back to main menu
+    elif choice1 = # name in list:
+        # select name
+    else:
+        # add name 
+
+# option 2
+# sort dictionary by donation amount
+
+# option 3
+# quit
