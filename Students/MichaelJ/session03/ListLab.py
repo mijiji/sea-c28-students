6066e73a9b0c3d2175f391a2090aac3782c5dde0
mijiji <michael.yu.ji@gmail.com>
2014-12-09 16:16:36 -0800

    list lab final fixes

--- Students/MichaelJ/session03/ListLab.py
+++ Students/MichaelJ/session03/ListLab.py
@@ -2,60 +2,58 @@
 list lab
 part 1
 '''
-list = ["Apples", "Pears", "Oranges", "Peaches"]
+list_ = ["Apples", "Pears", "Oranges", "Peaches"]
 
-print list
+print list_
 
 add = raw_input(u"add fruit ")
 
-list.append(add)
+list_.append(add)
 
-print list
+print list_
 
 pick = int(raw_input(u"give a number for that spot's fruit "))
 
 # print u"%i is in spot %s "%list[pick - 1], pick
-print u"{} is in spot {}".format(list[pick - 1], pick)
+print u"{} is in spot {}".format(list_[pick - 1], pick)
 
-print list
+print list_
 
-list.insert(0, "Durian")
+list_.insert(0, "Durian")
 
 print list
 
 print "fruits that start with P"
-for fruit in list:
+for fruit in list_:
     if fruit[0] == "P":
         print fruit
 
 # part 2
-list2 = list[:]
-list2.remove("Durian")
+list_.remove("Durian")
 
-print list2
+print list_
 
 vaporize = raw_input("Which fruit do you want to remove? ")
 
-list2.remove(vaporize)
+list_.remove(vaporize)
 
-print list2
+print list_
 
 # part 3
-list3 = list[:]
+list_[:]
 
-for fruit in list3:
+for fruit in list_[:]:
     like = raw_input(u"Do you like %s? " % fruit)
     if like == "no":
-        while fruit in list3:
-            list3.remove(fruit)
+        list_.remove(fruit)
     elif like == "yes":
         pass
     else:
         print "only yes or no"
-print list3
+print list_
 
 list4 = []
-for fruit in list:
+for fruit in list_:
     list4.append(fruit[::-1])
 
 print list4
