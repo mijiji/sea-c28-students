28cd1869f4f5ced55b96eadf3445f8597249ab6d
mijiji <michael.yu.ji@gmail.com>
2014-12-14 18:17:30 -0800

    count even bug fixes

--- Students/MichaelJ/session05/CountEvens.py
+++ Students/MichaelJ/session05/CountEvens.py
@@ -8,13 +8,13 @@ An exercise on list comprehensions to count even numbers in a list.
 
 
 def count_evens(nums):
-    """
+    count = sum([1 for x in nums if x % 2 == 0])
+    print count
+
+"""
 count = 0
     for num in nums:
         if num % 2 == 0:
             count += 1
 return count
-    """
-    sum([1 for x in nums if x % 2 == 0])
-
-print count_evens([2, 1, 2, 3, 4])
+"""
