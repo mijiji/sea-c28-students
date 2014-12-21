9ed5d26a217d00e68520923205d095d927080be3
mijiji <michael.yu.ji@gmail.com>
2014-12-21 13:59:34 -0800

    finished iterator

---
+++ Students/MichaelJ/session08/iterator.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+Simple iterator examples
+"""
+
+
+class IterateMe_1(object):
+    """
+    About as simple an iterator as you can get:
+    returns the sequence of numbers from zero to 4
+    ( like xrange(4) )
+    """
+    def __init__(self, stop=5):
+        self.current = -1
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.current += 1
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
+
+class IterateMe_2(object):
+    """
+    About as simple an iterator as you can get:
+    returns the sequence of numbers from zero to 4
+    ( like xrange(4) )
+    """
+    def __init__(self, start, stop, step=1):
+        self.start = start
+        self.stop = stop
+        self.step = 1
+        self.current = start - step
+
+    def __iter__(self):
+        return IterateMe_2(self.start, self.stop, self.step)
+
+    def next(self):
+        self.current += 1
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
+
+if __name__ == "__main__":
+
+    print("first version")
+    it = IterateMe_2(1, 5, 1)
+    for i in it:
+        if i > 3:
+            break
+        print i
+    for i in it:
+        print i
