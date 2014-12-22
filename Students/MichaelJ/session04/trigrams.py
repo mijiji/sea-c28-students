69170f805ca8c2af185f5be271664b61591021f9
mijiji <michael.yu.ji@gmail.com>
2014-12-21 19:39:55 -0800

    trigrams

--- Students/MichaelJ/session04/trigrams.py
+++ Students/MichaelJ/session04/trigrams.py
@@ -3,9 +3,15 @@ A method to creating new word combinations.
 
 Trigram
 
-Will use 3 wadjacent words, the first 2 words will be a key and
+Will use 3 adjacent words, the first 2 words will be a key and
 the 3rd word will be the possible outcome. Thus its purpose is to create
 pseudo books.
 """
 
 
+from nltk.util import ngrams
+text = "One night--it was on the twentieth of March, 1888"
+n = 3
+trigrams = ngrams(text.split(), n)
+for i in trigrams:
+    print i
