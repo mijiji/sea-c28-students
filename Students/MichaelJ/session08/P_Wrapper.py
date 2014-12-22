16258605b398a825c51b11c28569a53a102fd626
mijiji <michael.yu.ji@gmail.com>
2014-12-21 19:21:37 -0800

    finished p wrapper

--- Students/MichaelJ/session08/P_Wrapper.py
+++ Students/MichaelJ/session08/P_Wrapper.py
@@ -7,4 +7,14 @@ A wrapper to add to a function that returns a string.
 """
 
 
-def 
+def p_wrapper(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        result = "<p> %n </p>" % result
+        return result
+    return wrapper
+
+
+@p_wrapper
+def return_a_string(string):
+    return string
