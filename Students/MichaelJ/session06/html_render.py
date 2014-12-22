d9716708b6891a94c693e83a6243cdabbac2368c
mijiji <michael.yu.ji@gmail.com>
2014-12-21 22:54:42 -0800

    html render

--- Students/MichaelJ/session06/html_render.py
+++ Students/MichaelJ/session06/html_render.py
@@ -10,7 +10,7 @@ Can be used for presentation.
 
 class Element(object):
     tag = "html"
-    indent = "  "
+    indent = "    "
 
     def __init__(self, text=None, **kwargs):
         if not text:
@@ -27,23 +27,49 @@ class Element(object):
         file_out.write("\n")
         file_out.write(ind)
         file_out.write("<%s>"%self.tag
-        for content in self.info:
-            content.render(file_out, ind + self.indent)
+        for key, value in self.attributes.items():
+            file_out.write(' %s="%s" '%(key, value))
 
 
-class Html
+class Html(Element):
+    tag = "head"
+
 
 class Head(Element):
-    tag "head"
+    tag = "head"
+
 
 class Body(Element):
-    tag "body"
+    tag ="body"
+
 
 class P(Element):
-    tag "p"
+    tag = "p"
+
 
 class OneLineTag(Element):
     def render(self, file_out, ind=""):
-        file_out.write("\n")
-        file_out.write(ind)
-        file_out.write("</%s>"%self.tag)
+        file_out.write("%s<%s> %s </%s>\n" % (ind, self.tag, self.content[0], self.tag_name)
+
+
+class SelfClosingTag(Element):
+    def render(self, file_out, ind=""):
+        if self.content:
+            file_out.write("5s<5s<5s 5s/>\n") %(ind, self.tag_name,self.content[0]))
+        else file_out.write("%s<%s /n") % (ind, self.tag_name))
+
+
+class A(Element):
+    def __init__(self, link, content):
+        self.link = link
+        if content:
+            self.content = [content]
+        else.content = []
+
+
+class Ul(Element):
+    tag = "ul"
+
+
+class Li(Element):
+    tag = "Li"
