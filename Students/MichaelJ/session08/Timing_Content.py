7ae2e6fb448cf16f5755d02642a500fff7a6f574
mijiji <michael.yu.ji@gmail.com>
2014-12-21 17:44:39 -0800

    timing content bug fixes

--- Students/MichaelJ/session08/Timing_Content.py
+++ Students/MichaelJ/session08/Timing_Content.py
@@ -10,9 +10,23 @@ Will record and print the time elapsed to run through code.
 import time
 
 
-def timed_function():
+def sample():
     for i in range(100000):
         i = i ** 20
     return i
 
 
+class timer(object):
+    def __init__(self):
+        self.start = time.time()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        total_time = time.time() - self.start
+        print 'time elapsed was {} seconds'.format(total_time)
+
+with timer() as t:
+    for i in range(100000):
+        i = i ** 20
